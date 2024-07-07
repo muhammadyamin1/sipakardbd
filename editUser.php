4977ec901a09fb93bf33fd72dd3d9f35f9c9d6d4
Muhammad Yamin <muhammadyamin1081@gmail.com>
2024-07-07 23:46:48 +0700

    idUserAktif, include header, alert hapusUser, alert tambahUser, disable hapusUser, Tooltips, cekEmail

--- editUser.php
+++ editUser.php
@@ -1,11 +1,13 @@
 <?php
 include 'dbKoneksi.php';
 session_start();
+
 // Periksa apakah sudah ada sesi dan data yang disimpan
 if (isset($_SESSION['nama']) && isset($_SESSION['role'])) {
+  $idUserAktif = $_SESSION['idUser'];
   $nama = $_SESSION['nama'];
   $role = $_SESSION['role'];
-}else{
+} else {
   header("Location: index.php?pesan=belum-login");
   exit();
 }
@@ -15,6 +17,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $nama = $_POST['nama'];
   $email = $_POST['email'];
   $role = $_POST['role'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
+
+  if ($idUser == '14') {
+    $role = 'admin';
+  } else if ($_SESSION['role'] != 'admin') {
+    // Jika bukan admin, pastikan role tetap sebagai user
+    $role = 'user';
+  }
 
   // Cek apakah email sudah digunakan oleh pengguna lain
   $sql = "SELECT * FROM tb_user WHERE email='$email' AND idUser != '$idUser'";
@@ -23,8 +34,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if ($result->num_rows > 0) {
     $_SESSION['error'] = "Email sudah digunakan.";
   } else {
-    // Query untuk memperbarui data pengguna
-    $sql = "UPDATE tb_user SET nama='$nama', email='$email', role='$role' WHERE idUser='$idUser'";
+    // Jika password diisi, periksa apakah konfirmasi password cocok
+    if (!empty($password) && $password === $confirm_password) {
+      $hashed_password = hash('sha256', $password);
+      $sql = "UPDATE tb_user SET nama='$nama', email='$email', role='$role', password='$hashed_password' WHERE idUser='$idUser'";
+    } else if (empty($password)) {
+      $sql = "UPDATE tb_user SET nama='$nama', email='$email', role='$role' WHERE idUser='$idUser'";
+    } else {
+      $_SESSION['error'] = "Password dan konfirmasi password tidak cocok.";
+      header("Location: editUserForm.php?id=$idUser");
+      exit();
+    }
+
     if ($conn->query($sql) === TRUE) {
       $_SESSION['success'] = "Data berhasil di Edit.";
     } else {
@@ -36,4 +57,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   header("Location: editUserForm.php?id=$idUser");
   exit();
 }
-?>
